68e5840fc203f621fe269b1563c6179e25a38609
YeonuJung <gsh95214@naver.com>
2024-01-24 11:51:56 +0900

    자바스크립트 공부

--- js/기초개념.js
+++ js/기초개념.js
@@ -358,24 +358,24 @@ var dog = {
 };
 dog.speak();
 
-// 생성자 함수(틀, 껍데기 만드는) : 코드 한 줄로 유사한 객체(구조 똑같은) 여러개 만듬 
+// 생성자 함수(틀, 껍데기 만드는) : 코드 한 줄로 유사한 객체(구조 똑같은) 여러개 만듬
 // 특징 : 함수 이름의 첫 글자는 대문자로 시작하는게 관례, new 연산자를 붙여서 실행
 
 // 생성자 함수 -> function 함수이름에 대문자로 시작, 매개변수에 변수설정
 // 키값에는 this 붙여서, ":"" 대신 "=" 사용, 값에는 매개변수로 지정한 변수 지정
-function Animals(name, age){
-   this.name = name
-   this.age = age
+function Animals(name, age) {
+  this.name = name;
+  this.age = age;
 }
 
 // 코드 한 줄로 객체를 생성 -> new 연산자를 사용
 var dog = new Animals("하늘이", 18); //dog 객체를 생성
-var cat = new Animals("야옹이", 10); //cat 객체를 생성 
+var cat = new Animals("야옹이", 10); //cat 객체를 생성
 var tiger = new Animals("어흥이", 20);
 var lion = new Animals("캬앙이", 25);
-console.log(lion)
-var panda = new Animals("푸바오", 5); 
-console.log(panda)
+console.log(lion);
+var panda = new Animals("푸바오", 5);
+console.log(panda);
 /*
 var dog = {
    name : "하늘이",
@@ -395,33 +395,33 @@ var lion = {
 }
 */
 // 함수는 매개변수 개수에 따라서 같은 이름의 함수를 여러가지 만들 수 있음
-// 내장 객체 -> 자바스크립트에 내장되어 있는 객체 
+// 내장 객체 -> 자바스크립트에 내장되어 있는 객체
 // Date 객체 : 시간 정보를 담는 객체, 월(month)값은 0부터 시작
 var now = new Date();
-console.log(now)
+console.log(now);
 
 // Date 객체의 메소드는 여러 가지가 있다. (getDate(), getHours()......)
 var date = now.getDate();
-console.log(date)
+console.log(date);
 var hour = now.getHours();
-console.log(hour)
+console.log(hour);
 // 현재 우리나라 시간 값을 가진 객체를 생성하고 싶으면 toLocaleString() 사용
-console.log(now.toLocaleDateString()) // 날짜만
-console.log(now.toLocaleTimeString()) // 시간만
-console.log(now.toLocaleString()) //날짜, 시간 전부
+console.log(now.toLocaleDateString()); // 날짜만
+console.log(now.toLocaleTimeString()); // 시간만
+console.log(now.toLocaleString()); //날짜, 시간 전부
 
 // Math 객체 -> 수학 계산을 위한 객체, new Math()로 객체를 생성하지 않음
 // Math.프로퍼티 또는 Math.메소드()
 var sq = Math.sqrt(4); // 2
-var area1 = Math.PI*2*2; // 2 * 2 * 3.141592....
+var area1 = Math.PI * 2 * 2; // 2 * 2 * 3.141592....
 
 // 난수 발생 -> Math.random() 1보다 작은 0~1 사이의 실수를 리턴
 // Math.floor(m) -> m의 소수점 이하를 제거한 정수 리턴
 // 0~ 100 사이의 정수 10개를 랜덤하게 생성하려면?
-for(i = 0; i < 10; i++){
-   var m = Math.random()*100;
-   var n = Math.floor(m);
-   console.log(n)
+for (i = 0; i < 10; i++) {
+  var m = Math.random() * 100;
+  var n = Math.floor(m);
+  console.log(n);
 }
 
 // 함수의 구성 -> function 함수이름(arg1, arg2, ...argn){
@@ -429,13 +429,13 @@ for(i = 0; i < 10; i++){
 //              결과를 리턴하는 return문
 //               }
 // 함수특징 -> 코드 재사용이 가능하다, 코드 중복을 줄일 수 있다
-function adder(a, b){
+function adder(a, b) {
   var sum;
   sum = a + b;
   return sum;
 }
 var addN = adder(2000, 1500);
-console.log(addN)
+console.log(addN);
 // 자바스크립트에서 제공하는 전역함수 eval() 식을 계산하고 결과 리턴
 // parseInt() 문자열을 숫자로 바꿔주는 함수
 // isNaN() 숫자가 아님을 나타내는 함수 -> 숫자가 아니면 true 숫자이면 false
@@ -443,7 +443,7 @@ console.log(addN)
 // 다이얼로그 -> 사용자 입력 및 메시지 출력 (prompt, alert, confirm)
 // prompt("메시지", "디폴트 입력값(생략가능)") -> 사용자로부터 문자열을 입력 받아 리턴
 // 만약 취소 버튼이나 다이얼로그를 닫은 경우 -> null이라고 판단
-// 문자열 입력 없이 확인 버튼을 누른 경우 -> 빈 문자열로 인식 
+// 문자열 입력 없이 확인 버튼을 누른 경우 -> 빈 문자열로 인식
 
 // confirm("메시지") -> 메시지를 출력하고 확인 똬는 취소 버튼을 가진 다이얼로그 출력
 // 확인 버튼 누르면 true 리턴, 취소 버튼을 누르거나 강제로 닫으면 false 리턴
@@ -453,25 +453,25 @@ console.log(addN)
 // 화살표 함수 -> 함수 표현식보다 단순하고 간결한 문법으로 함수를 만들 수 있는 방법
 // let 함수이름 = (arg1, arg2....) => {
 //  프로그램 코드
-//  return 문   
+//  return 문
 // }
 let adder2 = (a, b) => {
   let sum = a + b;
   return sum;
-}
+};
 console.log(adder2(1, 3));
 // 이 화살표 함수를 원래 함수 표현식으로 나타내면?
-function adder3(a, b){
+function adder3(a, b) {
   var sum = a + b;
   return sum;
 }
-console.log(adder3(1, 3))
+console.log(adder3(1, 3));
 // 매개변수가 없는 경우 소괄호 생략 불가
 // 매개변수가 한 개인 경우 소괄호도 생략 가능, 코드가 한 줄인 경우 중괄호 생략 가능
 // let hi = () => {
 //   console.log("안녕하세요")
 // }
-let hi = () => console.log("안녕하세요")
+let hi = () => console.log("안녕하세요");
 
 // 고차함수 -> 인자로 함수를 받거나 함수를 반환하는 함수
 // 함수 안에서 다른 함수를 호출할 수 있는 함수
@@ -481,7 +481,7 @@ let hi = () => console.log("안녕하세요")
 const add = (a, b) => a + b;
 const sub = (a, b) => a - b;
 
-function calculator(a, b, calc){
+function calculator(a, b, calc) {
   let result = calc(a, b);
   console.log(result);
   return result;
@@ -490,8 +490,9 @@ function calculator(a, b, calc){
 calculator(1, 1, add);
 calculator(1, 2, sub);
 
+//setTimeout -> 함수, 시간  -> 함수를 내가 적은 시간을 기준으로 실행함.
 setTimeout(() => {
-  console.log("5초 후에 실행됩니다.")
+  console.log("5초 후에 실행됩니다.");
 }, 5000);
 
 // forEach() -> 콜백 함수를 배열의 각 요소에 대해 한 번씩 실행
@@ -502,47 +503,123 @@ setTimeout(() => {
 
 // forEach()를 이용한 배열 순회
 const numbers = [10, 20, 30, 40, 50];
-numbers.forEach(function(number){
-  console.log(number)
-})
+numbers.forEach(function (number) {
+  console.log(number);
+});
 
-numbers.forEach(number => console.log(number));
+numbers.forEach((number) => console.log(number));
 
-numbers.forEach((number, index) => console.log(
-  "Index : " + index + " Value " + number)
-)
+numbers.forEach((number, index) =>
+  console.log("Index : " + index + " Value " + number)
+);
 
-numbers.forEach((number, index, array) => console.log(array))
+numbers.forEach((number, index, array) => console.log(array));
 
 // find()함수 -> 조건을 만족하는 배열의 요소를 찾아서 반환해줌, 만족하는 거 찾는 순간 종료
 const fruits = ["apple", "banana", "melon", "orange", "melon"];
-const favorite2 = fruits.find(name => name === "melon");
-console.log(favorite2)
+const favorite2 = fruits.find((name) => name === "melon");
+console.log(favorite2);
 
 // findIndex() -> 조건을 만족하는 인덱스를 반환, 만족하는 거 찾는 순간 종료
-index2 = fruits.findIndex(name => name === "melon");
+index2 = fruits.findIndex((name) => name === "melon");
 console.log(index2);
 
 // some() -> 배열의 아이템들이 조건을 부분적으로 만족하는지 확인, true값 반환
-const some1 = fruits.some(function(name){
-  return name === "melon"
-})
-console.log(some1)
+const some1 = fruits.some(function (name) {
+  return name === "melon";
+});
+console.log(some1);
 
 //every() 배열의 아이템들이 조건을 전부 만족하는지 확인
 
 //filter() -> 조건에 맞는 모든 아이템들을 새로운 배열로 생성 -> 조건을 끝까지 다 봄
 // find랑 다르게 조건을 앞에서 만족한다고 해도 종료하지 않고 뒤에까지 다 살펴봄
-const filter1 = fruits.filter(function(name){
+const filter1 = fruits.filter(function (name) {
   return name === "melon";
-})
-console.log(filter1)
+});
+console.log(filter1);
 
 //map() -> 배열 내 모든 아이템에 콜백함수를 적용한 결과를 모아 새로운 배열을 반환
-// 현재 처리되고 있는 요소 뿐만 아니라 인덱스도 출력가능 
+// 현재 처리되고 있는 요소 뿐만 아니라 인덱스도 출력가능
 // forEach()에서 array만 제외하고 생각하면 됨.
-const array6 = [10, 20, 30, 40, 50]
-const map1 = array6.map(function(number){
-  return number * 3
-})
-console.log(map1)
+const array6 = [10, 20, 30, 40, 50];
+const map1 = array6.map(function (number) {
+  return number * 3;
+});
+console.log(map1);
+
+// 스프레드 연산자 -> ...(변수이름)
+// 점 세개를 찍어서 (배열, 객체, 함수의 매개변수)안에 담긴 여러 원소를 복사하는 기능을 가짐
+// 일일히 적는 무의미한 반복을 대신해주는 것이 스프레드 연산자
+function rest(num1, num2, ...nums) {
+  console.log(nums);
+}
+rest(10, 20, 30, 40, 50); // 실행결과 [30, 40, 50]
+// 즉 num1, num2에 10, 20이 차례로 먼저 인자로 전달되고 nums에 나머지 인자가 모두 들어가는 것임.
+// 이렇게 함수자리의 매개변수에 스프레드 연산자를 쓰고 싶으면 마지막에 적어주어야 함.
+// 마지막 자리에 들어간 스프레드 연산자를 레스트 파라미터라고 부름
+
+const num1 = [1, 2, 3];
+const num2 = [4, 5, 6];
+
+const newArr = num1.concat(num2);
+
+console.log(newArr); //[1, 2, 3, 4, 5, 6]
+// 이걸 스프레드 연산자로 만든다면?
+const newArr2 = [...num1, ...num2];
+console.log(newArr2);
+
+// 구조 분해 할당 -> 배열이나 객체의 속성을 해체해서 그 값을 개별 변수에 담는 것
+// [변수이름] = [저장하고 싶은 값]
+let a, b, rest2;
+[a, b] = [10, 20];
+console.log(a); //10
+console.log(b); //20
+[a, b, ...rest2] = [10, 20, 30, 40, 50];
+console.log(rest2);
+
+const foo = ["one", "two", "three"];
+const [red, yellow, green] = foo; //red yellow green 각각을 변수로 foo값 저장
+// 이렇게 하면 red yellow green 각각을 먼저 변수로 만들어 놓고 할 필요 없이
+// 선언과 동시에 값을 할당할 수 있음
+console.log(red);
+console.log(yellow);
+console.log(green);
+
+// Set 객체 -> 중복되지 않은 값들의 집합 (집합의 특성을 가짐)
+// 중복X, 순서도 없어서 인덱스로 값을 찾을 수도 X (배열과의 차이)
+
+const set1 = new Set(); // Set객체 생성
+console.log(set1); // Set(0) {}  -> Set 객체의 특징은 () 안에 데이터 개수를 표현해줌
+// Set 생성자 함수에 인자로 들어간 값이 Set객체 {} 안에 데이터로 저장됨
+// 인자에는 iterable객체만 들어갈 수 있음(배열, 문자열)
+
+const set2 = new Set([1, 2, 3, 4, 5]); // Set객체 생성
+console.log(set2); // Set(5) {1, 2, 3, 4, 5}
+
+const set3 = new Set("hello"); // Set객체 생성
+console.log(set3); // Set(4) {"h", "e", "l", "o"}
+// Set 객체는 중복을 허용 X 따라서 l이 하나만 들어가서 개수가 4개가 됨.
+// 또한 문자열을 넣으면 알파벳 단위로 끊어서 저장함
+
+// Set 객체에 적용할 수 있는 함수 -> new Set() -> Set객체 생성, size(사이즈 확인)
+// add(데이터 추가), forEach(데이터 순회), delete(삭제), clear(전부 삭제), has(데이터 존재 여부)
+const set = new Set([10, 20, 30]); // Set객체 생성
+console.log(set.size); //3
+console.log(set.has(10)); // true
+set.add(40);
+console.log(set); // {10, 20, 30, 40}
+set.delete(20);
+console.log(set); //{10, 30}
+set.clear();
+console.log(set);
+
+// Map 객체 -> 키와 값의 쌍으로 이루어진 객체, Map 객체는 key에 모든 자료형 가능
+const map = new Map(); // Map객체 생성
+map.set("name", "soo"); 
+map.set(1, "number"); 
+map.set(true, "trueValue");
+console.log(map); // 즉 객체와는 달리 map 객체의 키에는 모든 자료형이 가능
+// 만약 일반적인 객체였다면 키로는 문자열만 가능했겠지만 map 객체는 불리언, 숫자도 가능
+// Map 객체에 적용할 수 있는 함수 new Map() -> Map객체 생성 get(키) -> 키 값 출력
+// delete(삭제), set(키, 값) 키와 값 설정, size 사이즈 확인
