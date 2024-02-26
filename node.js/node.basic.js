5a45cfda78c33b6fa4405e509ca2dfa693fda0e4
YeonuJung <gsh95214@naver.com>
2024-02-26 14:36:56 +0900

    node.js 실습

---
+++ node.js/node.basic.js
@@ -0,0 +1,12 @@
+
+// const http = require('http')
+// http.createServer(function(request, response){
+//     response.writeHead(200, {'Content-Type' : 'text/html'});
+//     response.end('<h1>Hello World..!</h1>');
+// }).listen(3000,function(){
+//     console.log('Server running at http://127.0.0.1:3000');
+// });
+console.log('이름 : %s', '정정정')
+console.log('생년월일 : %d월 %d일', 10, 14)
+console.log('사는곳 : %s', '서울')
+console.log('취미 : %s, %s, %s', '노래', '자전거', '요리')
