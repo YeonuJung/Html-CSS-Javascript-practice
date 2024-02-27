1ef2c2fe9c0c9a87e7ec366f4fed94620282ee52
YeonuJung <gsh95214@naver.com>
2024-02-27 11:33:43 +0900

    node.js연습

--- node.js/node.basic.js
+++ node.js/node.basic.js
@@ -59,6 +59,7 @@ console.log(randoms[i])
 console.log(`제일 큰 수는 ${bigOfRandom(randoms)}입니다.`)
 */
 
+/*
 const crypto = require('crypto')
 const shaHash = crypto.createHash('sha256')
 shaHash.update('crypto_hash')
@@ -66,3 +67,33 @@ shaHash.update('crypto_hash')
 let output = shaHash.digest('hex')
 
 console.log('crypto_hash :', output)
+*/
+
+/*
+const crypto = require("crypto");
+const algorithm = "aes-256-cbc";
+const key = "abcdefghijklmnopqrstuvwxyz123456";
+const iv = "1234567890123456";
+const input = "암호화할 문장";
+// 암호화
+const cipher = crypto.createCipheriv(algorithm, key, iv);
+let cipheredOutput = cipher.update(input, "utf8", "base64");
+cipheredOutput += cipher.final("base64");
+// 복호화
+const decipher = crypto.createDecipheriv(algorithm, key, iv);
+let decipheredOutput = decipher.update(cipheredOutput, "base64", "utf8");
+decipheredOutput += decipher.final("utf8");
+// 출력
+console.log("원래 문자열 : " + input);
+console.log("암호화 : " + cipheredOutput);
+console.log("암호화 해제 : " + decipheredOutput)
+*/
+
+const myURL = new URL("https://sub.example.com:8080/p/a/t/h?query=string#hash");
+   console.log(myURL);
+   console.log(myURL.hostname);
+   console.log(myURL.port);
+   console.log(myURL.pathname);
+   console.log(myURL.search);
+   console.log(myURL.hash);
+   console.log(myURL.searchParams);
