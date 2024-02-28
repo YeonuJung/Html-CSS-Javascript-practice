8bbf05ab63d1375905764b3c224c01d9276427cc
YeonuJung <gsh95214@naver.com>
2024-02-28 09:52:53 +0900

    node.js연습

--- node.js/node.basic.js
+++ node.js/node.basic.js
@@ -161,3 +161,21 @@ fs.writeFile('output.txt', text, 'utf-8', (err) => {
 
 */
 
+// process 객체에 exit 이벤트 연결(exit은 노드실행 종료(함수 끝났을 때)시 자동으로 실행됨)
+process.on('exit', function(){
+    console.log('안녕히가세요^^');
+   });
+   // process 객체에 uncaughtException 이벤트 연결(예외, 즉 오류났을 때 실행)
+   process.on('uncaughtException', function(error){
+    console.log('예외가 발생했습니다!!');
+   });
+   // 2초 간격으로 3번 예외를 발생시킨다.
+   let count = 0;
+   function test() {
+    count = count + 1;
+    if(count > 3) { return; }
+    // 예외를 강제로 발생시킨다.
+    setTimeout(test, 2000);
+   error.error.error();
+   };
+   setTimeout(test, 2000);
