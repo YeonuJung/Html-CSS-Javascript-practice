aa5303a6fb51d412a796c5d7b21ab0d347b4f269
YeonuJung <gsh95214@naver.com>
2024-03-04 10:35:55 +0900

    node.js 연습

--- node.js/node.basic.js
+++ node.js/node.basic.js
@@ -633,3 +633,36 @@ app.listen(3000, function () {
   console.log("Server Running at http://127.0.0.1:3000");
 });
 */
+
+/*
+// 모듈 추출 및 서버 생성
+const express = require('express');
+const app = express();
+// 라우터 설정
+app.get('/page/:id', function(request, response){
+let name = request.params.id;
+// 응답
+response.send('<h1>' + name + ' Page</h1>');
+});
+// 서버 실행
+app.listen(3000, function () {
+console.log("Server Running at http://127.0.0.1:3000");
+});
+*/
+
+/*
+// 모듈 추출 및 서버 생성
+const express = require('express');
+const app = express();
+// 라우터 설정
+app.get('/', function(request, response){
+response.send('<h1>Index Page</h1>');
+});
+app.all('*', function(request, response){
+response.status(404).send('<h1>ERROR-Page Not Found</h1>');
+});
+// 서버 실행
+app.listen(3000, function(){
+console.log("Server Running at http://127.0.0.1:3000");
+});
+*/
